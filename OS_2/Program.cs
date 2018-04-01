2470c7d710a3988aa88357d7d24359dab04effdb
Евгений Меркулов <i_zhenya.z@mail.ru>
2018-04-01 23:00:56 +0300

    Initital

---
+++ OS_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OS_2
+{
+    class Program
+    {
+        static bool thread1 = true, thread2 = true;
+        static Semaphore sem = new Semaphore(1,1);
+        static void One()
+        {
+            while (thread1)
+            {
+                sem.WaitOne();
+                for (int i = 0; i < 5; i++)
+                    Console.Write('1');
+                Thread.Sleep(200);
+                sem.Release();
+            }
+        }
+        static void Two()
+        {
+            while (thread2)
+            {
+                sem.WaitOne();
+                for (int i = 0; i < 5; i++)
+                    Console.Write('2');
+                Thread.Sleep(200);
+                sem.Release();
+            }                
+        }
+        static void Main(string[] args)
+        {
+            Thread firstthread = new Thread(One);
+            firstthread.Start();
+            Thread secondthread = new Thread(Two);
+            secondthread.Start();
+            Console.ReadKey();
+            thread1 = thread2 = false;
+            Console.WriteLine("Threads had stopped.");
+            Console.ReadKey();
+        }
+    }
+}
